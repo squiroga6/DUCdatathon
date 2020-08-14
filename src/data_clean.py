1bda7204adbcda2248d1ed17ea7ba38bb0bfeb0e
squiroga <samuel.quiroga@cenovus.com>
2020-08-14 11:30:53 -0600

    predicted output

--- src/data_clean.py
+++ src/data_clean.py
@@ -3,46 +3,60 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn import metrics
 
 # import well header data
 well_header = pd.read_csv("../data/WellHeader_Datathon.csv")
 
 #select colummns of interest
-well_header_clean = well_header[['TVD','TotalDepth','Formation',
-                                 'Surf_Longitude','Surf_Latitude',
-                                 'BH_Longitude','BH_Latitude',
-                                 'WellProfile','Field','KBElevation']]
-# drop na
-well_header_clean = well_header_clean.dropna()
-# remove vertical wells
-# well_header_clean = well_header_clean[well_header_clean.WellProfile != "Vertical"]
-# label field column
-well_header_clean['Field'] = well_header_clean['Field'].astype('category')
-# well_header_clean.info()
-well_header_clean['Field_code'] = well_header_clean['Field'].cat.codes
+well_header = well_header[[
+                        'EPAssetsId',
+                        'TVD','TotalDepth','Formation',
+                        'Surf_Longitude','Surf_Latitude',
+                        # 'BH_Longitude','BH_Latitude',
+                        'WellProfile','Field','KBElevation'
+                        ]]
+
+# get submission ids for testing
+submission_sample = pd.read_csv("../data/Submission_Sample.csv")
+EPAssetId_submission = submission_sample['EPAssetsId']
+
+# filter well header to only include training/cv (non-na TVD) plus final test
+well_header = well_header[
+      (well_header['EPAssetsId'].isin(EPAssetId_submission)) | (pd.notna(well_header.TVD))
+      ]
+
+# check if missing ids are equal to submission test: want TRUE
+missingID = well_header[pd.isna(well_header.TVD)].EPAssetsId.reset_index(drop=True)
+missingID.sort_values(ignore_index=True).equals(EPAssetId_submission.sort_values(ignore_index=True))
+
+
+# label field column (for cluster metrics comparisons)
+well_header['Field'] = well_header['Field'].astype('category')
+well_header['Field_code'] = well_header['Field'].cat.codes
 
 # quick plot
 plt.figure(figsize = (15,8))
-sns.scatterplot(well_header_clean['Surf_Longitude'], 
-                well_header_clean['Surf_Latitude'],alpha=0.2,
-                hue=well_header_clean['Field'],legend=False)
+sns.scatterplot(well_header['Surf_Longitude'], 
+                well_header['Surf_Latitude'],alpha=0.2,
+                hue=well_header['Field'],legend=False)
 
-# Compute DBSCAN
+# Compute DBSCAN clustering
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
 
 # cluster lat long
-from sklearn.cluster import DBSCAN
-lat_longs = well_header_clean[['Surf_Longitude','Surf_Latitude']]
+lat_longs = well_header[['Surf_Longitude','Surf_Latitude']]
 db = DBSCAN(eps=0.1, min_samples=10).fit(lat_longs)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
-labels_true = np.array(well_header_clean.Field_code)
+labels_true = np.array(well_header.Field_code)
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
 
+# print out metrics
 print('Estimated number of clusters: %d' % n_clusters_)
 print('Estimated number of noise points: %d' % n_noise_)
 print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
@@ -55,24 +69,28 @@ print("Adjusted Mutual Information: %0.3f"
 print("Silhouette Coefficient: %0.3f"
       % metrics.silhouette_score(lat_longs, labels))
 
-
+# re-run with prediction values
 db = DBSCAN(eps=0.1, min_samples = 10)
 clusters = db.fit_predict(lat_longs)
 
 # quick plot with clusters
 plt.figure(figsize = (15,8))
-sns.scatterplot(well_header_clean['Surf_Longitude'], 
-                well_header_clean['Surf_Latitude'],
+sns.scatterplot(well_header['Surf_Longitude'], 
+                well_header['Surf_Latitude'],
                 alpha=0.2,hue=clusters,palette="plasma")
 
+# check length of clusters
 len(np.unique(np.array(clusters)))
 
 # add clusters back to df
-well_header_clean['DBSCAN_Clusters']=clusters
+well_header['DBSCAN_Clusters']=clusters
 
 #select colummns of interest
-well_header_clean = well_header_clean[['TVD','TotalDepth','Formation',
-                                       'WellProfile','DBSCAN_Clusters',
-                                       'KBElevation']]
-
-well_header_clean = well_header_clean[well_header_clean['DBSCAN_Clusters']>1]
+well_header_clean = well_header[[
+                              'EPAssetsId',
+                              'TVD','TotalDepth','Formation',
+                              'WellProfile','DBSCAN_Clusters',
+                              'KBElevation'
+                              ]]
+# remove "noise" clusters
+# well_header_clean = well_header[well_header['DBSCAN_Clusters']>-1]
--- src/random_forests.py
+++ src/random_forests.py
@@ -5,7 +5,21 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 # explore clean dataset (from data_clean.py)
-well_header_clean.head()
+well_header_clean.sample(10)
+
+
+well_header_validation = well_header_clean[pd.isna(well_header_clean.TVD)]
+# get unique values of formation, clusters, and well profile 
+# from validation set to filter the training set
+validation_clusters = well_header_validation.DBSCAN_Clusters.unique()
+validation_formation = well_header_validation.Formation.unique()
+validation_profile = well_header_validation.WellProfile.unique()
+
+# filter training set
+well_header_train = well_header_clean[pd.notna(well_header_clean.TVD)][
+        well_header_clean.DBSCAN_Clusters.isin(validation_clusters)][
+        well_header_clean.Formation.isin(validation_formation)][
+        well_header_clean.WellProfile.isin(validation_profile)]
 
 # select features and target
 features = [
@@ -17,8 +31,9 @@ features = [
 
 target = ['TVD']
 
-X = well_header_clean[features]
-y = well_header_clean[target]
+X = well_header_train[features]
+
+y = well_header_train[target]
 
 # hot encode
 X = pd.get_dummies(X, 
@@ -28,13 +43,17 @@ X = pd.get_dummies(X,
                             "DBSCAN_Clusters"], 
                     prefix=["fm", "profile","dbscan_cluster"])
 
-X.drop('profile_Directional',axis=1,inplace=True)
+# X.drop('profile_Directional',axis=1,inplace=True)
 
 # split training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=324)
 
 # fit regressor
-regr = RandomForestRegressor(max_depth=50, random_state=0)
+regr = RandomForestRegressor(
+                        # max_depth=10, 
+                        # random_state=0
+                        # max_samples=5970
+                        )
 regr.fit(X, y)
 
 # get prediction
@@ -43,3 +62,23 @@ y_prediction = regr.predict(X_test)
 # get RMSE
 RMSE = sqrt(mean_squared_error(y_true = y_test, y_pred = y_prediction))
 print(RMSE)
+
+X_validation = well_header_validation[features]
+# hot encode
+X_validation = pd.get_dummies(X_validation, 
+                    columns=[
+                            "Formation", 
+                            "WellProfile",
+                            "DBSCAN_Clusters"], 
+                    prefix=["fm", "profile","dbscan_cluster"])
+
+# X_validation.drop('profile_Directional',axis=1,inplace=True)
+
+# get prediction
+y_validation = regr.predict(X_validation)
+
+well_header_submission = well_header_validation['EPAssetsId'].to_frame()
+
+well_header_submission['Predicted_TVD'] = y_validation
+
+well_header_submission.to_csv("../predictions/predicted_tvd.csv")
